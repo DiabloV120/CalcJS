7ff077c03799fc27dc86fc596cb88cd7a82cbdc5
DiabloV120 <artyom.dyakonov120@gmail.com>
2023-02-07 13:19:24 +0300

    changes

---
+++ App.js
@@ -0,0 +1,19 @@
+let name = document.querySelector("#name");
+let price = document.querySelector("#price");
+let amount = document.querySelector("#amount");
+let add = document.querySelector("#add");
+let table = document.querySelector("#table");
+let total = document.querySelector("#total");
+
+function createCell(tr, value, name) {
+  add.addEventListener("click", function () {
+    let tr = document.createElement("tr");
+    createCell(tr, name.value, "name");
+    createCell(tr, price.value, "price");
+    createCell(tr, amount.value, "amount");
+    createCell(tr, price.value * amount.value, "cost");
+    createCell(tr, "удалить", "remove");
+    table.appendChild(tr);
+  });
+}
+
